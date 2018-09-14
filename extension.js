11a001a43fb6abf141457b7c2aaa11583e37b2c6
Facundo Espinosa <facundo@backlotcars.com>
2018-09-13 22:55:57 -0300

    add main code of extension.js

--- extension.js
+++ extension.js
@@ -1,30 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+'use strict'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
+const vscode = require('vscode')
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "m-vscode-runner" is now active!');
+exports.activate = function activate(context) {
+  let disposable = vscode.commands.registerCommand('extension.runTest', function () {
+    let currentEditor = vscode.window.activeTextEditor
+    if(!currentEditor) { return }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    const startPath = "/test/"
+    const currentFilePath = `${startPath.substring(1)}${currentEditor.document.fileName.split(startPath)[1]}`
+    const currentLine     = currentEditor.selection.active.line + 1
+    const terminal        = vscode.window.createTerminal()
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    terminal.show()
+    terminal.sendText(`m ${currentFilePath}:${currentLine}`)
+    vscode.window.showInformationMessage('')
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
-exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {
+ exports.deactivate = function deactivate() {
 }
-exports.deactivate = deactivate;
